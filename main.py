2f0ea4d495351d9317a606e9619b01e3524691b6
Anthony Wynne <12174019+ant358@users.noreply.github.com>
2023-11-09 00:15:54 +0000

    basic main written

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import src.find_similar_docs as fsd
 
 
 print("Loading dataset...")
-dataset=pd.read_csv("data/dataset_rent_rome_kijiji.tsv", sep="\t")
-dataset['doc_id']=dataset.index
+dataset = pd.read_csv("data/dataset_rent_rome_kijiji.tsv", sep="\t")
+print(dataset.head())
+print(dataset.info())
+dataset['doc_id'] = dataset.index
 doc_nr = dataset['doc_id'].max()
 print("Dataset loaded correctly.")
 print("Producing Shingles...")
 start_time = time.time()
-#an array where the index i represent the document_id and the element shingling_list[i] the hashed shingles for document document_id
-shingling_list = [None] * (doc_nr +1) 
+shingling_list = [None] * (doc_nr + 1)
 shingling_size = 10
 signature_size = 50
 bands_nr = 10
@@ -20,12 +21,13 @@ shingler_inst = fsd.shingler(shingling_size)
 signer = fsd.minhashSigner(signature_size)
 
 
-#produce hashed shinglings for all documents
+# produce hashed shinglings for all documents
 for index, row in dataset.iterrows():
-    doc = row['Title']+" "+row['Short Description']
+    doc = row['Title'] + " " + row['Short Description']
     i = row['doc_id']
-    
-    shinglings = shingler_inst.get_hashed_shingles( shingler_inst.get_shingles(doc) )
+
+    shinglings = shingler_inst.get_hashed_shingles(
+        shingler_inst.get_shingles(doc))
     shingling_list[i] = shinglings
 
 end_time = time.time()
@@ -34,10 +36,58 @@ print("Shingles produced in:\t %.2f seconds."%(end_time - start_time))
 
 start_time = time.time()
 print("Computing signature matrix...")
-#produce a signature for each shingle set
-signature_matrix = signer.compute_signature_matrix( shingling_list )
+# produce a signature for each shingle set
+signature_matrix = signer.compute_signature_matrix(shingling_list)
 end_time = time.time()
-print("Signature Matrix computed in:\t %.2f seconds." %(end_time - start_time))
+print("Signature Matrix computed in:\t %.2f seconds." %
+      (end_time - start_time))
 
 
+lsh_instance = fsd.lsh(threshold=0.8)
+start_time = time.time()
+print("Computing LSH similarity...")
+lsh_similar_itemset = lsh_instance.get_similar_items(signature_matrix, bands_nr, signature_size)
+end_time = time.time()
+lsh_computation_time = end_time - start_time
+print(
+    "LSH Similarity computed in:\t %.2f seconds.\nSimilar Elements Found: %d" %
+    (lsh_computation_time, len(lsh_similar_itemset)))
 
+
+# find all the matching doc_id for a given doc_id
+def find_matching_doc(doc_id):
+    return [couple for couple in lsh_similar_itemset if doc_id in couple]
+
+
+def get_matching_doc_set(doc_id):
+    sim_docs = find_matching_doc(doc_id)
+    sim_docs_set = set()
+    for couple in sim_docs:
+        sim_docs_set.add(couple[0])
+        sim_docs_set.add(couple[1])
+
+    return sim_docs_set
+
+
+# add a new column to the dataset containing the set of similar documents
+dataset['similar_docs'] = dataset['doc_id'].apply(get_matching_doc_set)
+dataset['num_similar_docs'] = dataset['similar_docs'].apply(len)
+
+# dataset sort by number of similar documents
+dataset = dataset.sort_values(by=['num_similar_docs'], ascending=False)
+
+print(dataset[['Title', 'similar_docs', 'num_similar_docs']].head())
+
+# output the dataset to a file
+dataset.to_csv("output/dataset_rent_rome_kijiji_similar.tsv", sep="\t")
+# reduce the dataset to only one version of each similar document
+dataset_reduced = dataset.drop_duplicates(subset=['similar_docs'],
+                                          keep='first')
+dataset_reduced.to_csv("output/dataset_rent_rome_kijiji_similar_reduced.tsv",
+                       sep="\t")
+print(dataset_reduced.info())
+# show reduction ratio
+print("Reduction ratio: %.2f%%" %
+      (100 * (1 - dataset_reduced.shape[0] / dataset.shape[0])))
+
+print("done")
--- src/find_similar_docs.py
+++ src/find_similar_docs.py
@@ -1,6 +1,8 @@
-import pandas as pd
-import re, hashlib, math, time
+import re
+import hashlib
+import math
 from random import randint, seed
+
 seed(1631996)
 
 
@@ -9,41 +11,49 @@ class hashFamily:
         # how many bytes we want back
         self.resultSize = 8
         # how long can our i be (in decimal)
-        self.maxLen = 20 
+        self.maxLen = 20
         self.salt = str(i).zfill(self.maxLen)[-self.maxLen:]
-        
+
     def get_hash_value(self, el_to_hash):
-        return int(hashlib.sha1(str(el_to_hash).encode('utf-8') + self.salt.encode('utf-8')).hexdigest()[-self.resultSize:], 16)
+        return int(
+            hashlib.sha1(
+                str(el_to_hash).encode("utf-8") + self.salt.encode("utf-8")
+            ).hexdigest()[-self.resultSize:],
+            16,
+        )
+
 
 class shingler:
     def __init__(self, k):
-        
         self.k = int(k) if k > 0 else 10
-        
-    #inner class utility
+
+    # inner class utility
     def process_doc(self, document):
-        return re.sub("( )+|(\n)+"," ",document).lower()
-    
+        return re.sub("( )+|(\n)+", " ", document).lower()
+
     def get_shingles(self, document):
-        document= self.process_doc(document)
-        return {document[i:i+self.k] for i in range(len(document)-self.k+1)}
-    
+        document = self.process_doc(document)
+        return {
+            document[i:i + self.k]
+            for i in range(len(document) - self.k + 1)
+        }
+
     def get_k(self):
         return self.k
-    
-    #return sorted hash
+
+    # return sorted hash
     def get_hashed_shingles(self, shingles_set):
         hash_function = hashFamily(0)
-        return sorted( {hash_function.get_hash_value(s) for s in shingles_set} )
+        return sorted({hash_function.get_hash_value(s) for s in shingles_set})
 
- 
+
 class minhashSigner:
     def __init__(self, sig_size):
-        self.sig_size=sig_size
+        self.sig_size = sig_size
         self.hash_functions = [
             hashFamily(randint(0, 10000000000)) for _ in range(sig_size)
         ]
-    
+
     def compute_set_signature(self, set_):
         set_sig = []
         for h_funct in self.hash_functions:
@@ -52,94 +62,90 @@ class minhashSigner:
                 h = h_funct.get_hash_value(el)
                 if h < min_hash:
                     min_hash = h
-                
+
             set_sig.append(min_hash)
-        
+
         return set_sig
-    
-    #return a list of lists that can be seen as the signature matrix
+
+    # return a list of lists that can be seen as the signature matrix
     def compute_signature_matrix(self, set_list):
         return [self.compute_set_signature(s) for s in set_list]
+
 
 class lsh:
     def __init__(self, threshold=0.8):
         self.threshold = threshold
-        
-    def get_signature_matrix_bands(self, sig_matrix, bands_nr, sign_len): 
-        #bands_nr = b
-        #sign_len = n
-        r = int(sign_len/bands_nr) #number of rows in each band
-        bands = {} # {band_nr: [col_1,col_2,...]} where col_1 is all the values of Sig(S_i) for band b.
-        for i in range(0,bands_nr):
-            bands[i] = []
-        
-        # put Subsets of the columns of signature matrix into the appropriate bucket and cosider a column 
-        # as a unique block so that we can hash the entire column.
-        # Basically a band is a list of element, where each element is a subset of a signature of a given set.
-        for signature in sig_matrix: 
-            
-            for i in range(0, bands_nr):
-                idx = i*r    
-                bands[i].append(' '.join(str(x) for x in signature[idx:idx+r]) ) 
-                    
+
+    def get_signature_matrix_bands(self, sig_matrix, bands_nr, sign_len):
+        # bands_nr = b
+        # sign_len = n
+        r = int(sign_len / bands_nr)
+        bands = {i: [] for i in range(bands_nr)}
+        for signature in sig_matrix:
+            for i in range(bands_nr):
+                idx = i * r
+                bands[i].append(" ".join(
+                    str(x) for x in signature[idx:idx + r]))
+
         return bands
 
-    #band is a list 
-    # construct a dictionary {hash(band_column): doc_id that produced this hash}
     def get_band_buckets(self, band, hash_funct):
         buckets = {}
-        for doc_id in range(0,len(band)):
-            value = hash_funct.get_hash_value( band[doc_id] )
+        for doc_id in range(len(band)):
+            value = hash_funct.get_hash_value(band[doc_id])
             if value not in buckets:
                 buckets[value] = [doc_id]
             else:
-                 buckets[value].append(doc_id)
-                
+                buckets[value].append(doc_id)
+
         return buckets
-    
+
     def get_candidates_list(self, buckets):
         candidates = set()
-        # buckets is a dictionary containing key=bucket, value= list of doc_ids that hashed to bucket
-        for bucket,candidate_list in buckets.items():
+        for bucket, candidate_list in buckets.items():
             if len(candidate_list) > 1:
-                for i in range(0,len(candidate_list)-1):
-                    for j in range(i+1,len(candidate_list)):  
-                        pair = tuple(sorted( (candidate_list[i],candidate_list[j]) ))
+                for i in range(len(candidate_list) - 1):
+                    for j in range(i + 1, len(candidate_list)):
+                        pair = tuple(
+                            sorted((candidate_list[i], candidate_list[j])))
                         candidates.add(pair)
-                
-        return candidates #ie a set of couples, each couple is a candidate pair
-    
+
+        return candidates
+
     def check_candidates(self, candidates_list, threshold, sigs):
-        similar_docs = set() #set of tuples
-        # similar_pair is a couple containing doc_ids of documents that hashed to same bucket
-        for  similar_pair in candidates_list:
-            #for all the pairs of document in the list check similarity of their signatures
+        similar_docs = set()  # set of tuples
+        for similar_pair in candidates_list:
             doc_id_1 = similar_pair[0]
             doc_id_2 = similar_pair[1]
-            signature_1 = set(sigs[doc_id_1]) #get the i-th column from signature matrix where i is doc_id in the collision list
+            signature_1 = set(
+                sigs[doc_id_1]
+            )
             signature_2 = set(sigs[doc_id_2])
-            js = len(signature_1.intersection(signature_2)) /len(signature_1.union(signature_2))
-            
+            js = len(signature_1.intersection(signature_2)) / len(
+                signature_1.union(signature_2))
+
             if js >= threshold:
-                similar_docs.add( tuple(sorted((doc_id_1,doc_id_2) )) )
-                        
-                        
+                similar_docs.add(tuple(sorted((doc_id_1, doc_id_2))))
+
         return similar_docs
-    
+
     def get_similar_items(self, sig_matrix, bands_nr, sign_len):
         similar_docs = set()
-        #divide signature matrix into bands
-        bands = lsh_instance.get_signature_matrix_bands(sig_matrix,bands_nr,sign_len)
-        
-        #for all the bands
-        for band_id, elements in bands.items():
-            #produce the buckets for the given band (band_id) with a random hash function
-            buckets = lsh_instance.get_band_buckets(elements, hash_funct=hashFamily(randint(0,10000000000)))
-            #Get all the candidate pairs
-            candidates = lsh_instance.get_candidates_list(buckets)
-            #Check all candidate pairs' signatures
-            for sim_tuple in lsh_instance.check_candidates(candidates, self.threshold, sig_matrix):
-                similar_docs.add( sim_tuple)
+        # divide signature matrix into bands
+        bands = self.get_signature_matrix_bands(
+            sig_matrix, bands_nr, sign_len)
 
-        return similar_docs #return all the similar signatures that respect the threshold
-                
+        # for all the bands
+        for band_id, elements in bands.items():
+            buckets = self.get_band_buckets(
+                elements, hash_funct=hashFamily(randint(0, 10000000000)))
+            # Get all the candidate pairs
+            candidates = self.get_candidates_list(buckets)
+            # Check all candidate pairs' signatures
+            for sim_tuple in self.check_candidates(
+                    candidates, self.threshold, sig_matrix):
+                similar_docs.add(sim_tuple)
+
+        return (
+            similar_docs
+        )
